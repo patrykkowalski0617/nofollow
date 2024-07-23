405fcde7e1e3f9dda16a8d0415fc6d54f145015b
Patryk <38049103+patrykkowalski0617@users.noreply.github.com>
2024-07-23 13:03:31 +0200

    Add files via upload

---
+++ nofollow/content.js
@@ -0,0 +1,19 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+window.addEventListener("keypress", (e) => {
+  if (e.code === "KeyQ" && e.ctrlKey) {
+    const aElements = document.querySelectorAll("a");
+    aElements.forEach((aElement) => {
+      if (aElement.rel === "nofollow") {
+        aElement.style.outline = "2px solid red";
+      }
+    });
+  }
+});
+
+/******/ })()
+;
+//# sourceMappingURL=content.js.map
